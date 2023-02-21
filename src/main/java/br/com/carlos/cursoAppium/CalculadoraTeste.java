808f6e7d448f8f5f6edf80169655b3a75f5880d7
Karl Otaner <carlosrnsilva01@gmail.com>
2023-02-21 00:07:36 -0300

    Instalaçao do APK via Appium

--- src/main/java/br/com/carlos/cursoAppium/CalculadoraTeste.java
+++ src/main/java/br/com/carlos/cursoAppium/CalculadoraTeste.java
@@ -2,9 +2,8 @@ package br.com.carlos.cursoAppium;
 
 import io.appium.java_client.MobileElement;
 import io.appium.java_client.android.AndroidDriver;
-import org.junit.Assert;
+import io.appium.java_client.remote.MobileCapabilityType;
 import org.junit.Test;
-import org.junit.Assert.*;
 import org.openqa.selenium.remote.DesiredCapabilities;
 
 import java.net.MalformedURLException;
@@ -14,26 +13,18 @@ import java.net.URL;
 public class CalculadoraTeste {
 
         @Test
-        public void deveSomarDoisValores() throws MalformedURLException {
+        public void deveInstalarApk() throws MalformedURLException {
         DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
         desiredCapabilities.setCapability("platformName", "Android");
         desiredCapabilities.setCapability("deviceName", "Nexus5-teste");
         desiredCapabilities.setCapability("udid", "emulator-5554");
-        desiredCapabilities.setCapability("automationName", "uiautomator2");
-        desiredCapabilities.setCapability("appPackage", "com.android.calculator2");
-        desiredCapabilities.setCapability("appActivity", "com.android.calculator2.Calculator");
+        //desiredCapabilities.setCapability("automationName", "uiautomator2");
+        //desiredCapabilities.setCapability("appPackage", "com.android.calculator2");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\carlos.silva\\WS\\CursoTesteAndroidAppium\\CursoAppium\\src\\main\\resources\\CTAppium_2_0.apk");
+
 
         AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
 
-        MobileElement el3 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_2");
-        el3.click();
-        MobileElement el4 = (MobileElement) driver.findElementByAccessibilityId("plus");
-        el4.click();
-        MobileElement el5 = (MobileElement) driver.findElementById("com.android.calculator2:id/digit_2");
-        el5.click();
-        MobileElement el6 = (MobileElement) driver.findElementById("com.android.calculator2:id/result");
-        Assert.assertEquals("4", el6.getText());
-        System.out.println(el6);
 
         driver.quit();
     }
