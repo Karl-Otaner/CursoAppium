5f0c200f3610b491b96c3c4e16ff65eb667223b7
Karl Otaner <carlosrnsilva01@gmail.com>
2023-02-22 01:31:37 -0300

    Validando Combobox

--- src/main/java/br/com/carlos/cursoAppium/FormularioTeste.java
+++ src/main/java/br/com/carlos/cursoAppium/FormularioTeste.java
@@ -47,4 +47,38 @@ public class FormularioTeste {
         driver.quit();
 
     }
+
+    @Test
+    public void deveInteragirCombo() throws MalformedURLException {
+        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+        desiredCapabilities.setCapability("platformName", "Android");
+        desiredCapabilities.setCapability("deviceName", "Nexus5-teste");
+        desiredCapabilities.setCapability("udid", "emulator-5554");
+        desiredCapabilities.setCapability("automationName", "uiautomator2");
+        desiredCapabilities.setCapability(MobileCapabilityType.APP, "C:\\Users\\carlos.silva\\WS\\CursoTesteAndroidAppium\\CursoAppium\\src\\main\\resources\\CTAppium_2_0.apk");
+
+
+        AndroidDriver<MobileElement> driver = new AndroidDriver(new URL("http://127.0.0.1:4723/wd/hub"), desiredCapabilities);
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        //Selecionar formulario
+        driver.findElement(By.xpath("//android.widget.TextView[@text='Formulário']")).click();
+
+        //Clicar no combo
+        driver.findElement(MobileBy.AccessibilityId("console")).click();
+
+        //Selecionar opção desejada
+        driver.findElement(By.xpath("//android.widget.CheckedTextView[@text='Nintendo Switch']")).click();
+
+
+        //Verificar opção selecionada
+        String text = driver.findElement(By.xpath("//android.widget.Spinner/android.widget.TextView")).getText();
+        Assert.assertEquals("Nintendo Switch", text);
+
+
+
+
+        driver.quit();
+
+    }
 }
